fe0bba5ae6482fc2b4b403ea4b5dc2826e9b01f9
Ackson Mutuma <78912538+Ackson94@users.noreply.github.com>
2023-06-09 18:44:21 +0200

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    len_sen = len(sentence)
+    length = len(sentence)
 
-    if (len_sen == 0):
-        new_tuple = (len_sen, None)
+    if (length == 0):
+        sam = (length, None)
     else:
-        new_tuple = (len_sen, sentence[0])
+        sam = (length, sentence[0])
 
-    return (new_tuple)
+    return (sam)
